e25d863e933072cabf5addeba8a6a720b5863473
胡琤波 <657225485@qq.com>
2024-08-27 13:45:35 +0800

    export { ConsoleLog78, IConsoleLog78 };

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 ﻿import { TsLog78 } from './TsLog78';
+import ConsoleLog78 from './ConsoleLog78';
+import IConsoleLog78 from './IConsoleLog78';
 
+export { ConsoleLog78, IConsoleLog78 };
 export default TsLog78;
